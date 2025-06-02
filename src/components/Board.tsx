6cb9355fee542884e0a4f2a6d482483bc7c3965c
filiplyrheden <filip.lyrheden@gmail.com>
2025-06-02 20:54:04 +0200

    Created shuffleArray function to randomize the squares

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,15 +1,18 @@
 import Square from "./Square";
 import "./Board.css";
+import { shuffleArray } from "../utils/shuffle";
 
 export default function Board() {
   const totalSquares = 3 * 5;
-  const tiles = Array.from({ length: totalSquares }, (_, i) =>
+  const square = Array.from({ length: totalSquares }, (_, i) =>
     i === totalSquares - 1 ? null : i + 1
   );
 
+  const shuffledSquares = shuffleArray(square);
+
   return (
     <div className="board" style={{ gridTemplateColumns: `repeat(5, 64px)` }}>
-      {tiles.map((square, i) => (
+      {shuffledSquares.map((square, i) => (
         <Square key={i} value={square} />
       ))}
     </div>
